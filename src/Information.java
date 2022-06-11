76774aae9872731170dff51ecb9a7626987af4ca
Mohammad Alshareef <80102737+mohammad2002m@users.noreply.github.com>
2022-06-11 18:09:19 +0300

    Update Information.java

--- src/Information.java
+++ src/Information.java
@@ -5,7 +5,7 @@ abstract public class Information {
 	public static int Entered = 0 , Pay = 0 , Selected = -1; 
 	public static int Coins_Entered[] = {0 , 0 , 0 , 0}; // The Coins that the custumer Entered
 	public static int Coins_In_Machine[] = {1 , 1 , 1 , 1}; // number of coins int the machine
-	public static int Val[] = {1 , 2 , 5 , 10}; // just the valuse of the coins
+	public static int Val[] = {1 , 2 , 5 , 10}; // just the values of the coins
 	public static ArrayList<Water> A = new ArrayList<>();
 	public static ArrayList<SoftDrinks> B = new ArrayList<>();
 	public static ArrayList<KidsJuices> C = new ArrayList<>();
